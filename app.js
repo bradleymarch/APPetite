b622c2aee702ff467b49d5fe2ca37bf221931e18
bradleymarch <bradleymarch@gmail.com>
2017-04-07 11:20:11 -0500

    first commit

--- app.js
+++ app.js
@@ -1,165 +1,95 @@
 $(function() {
 
-  //$(".js-home-view").hide();
-  $(".js-home-view").show();
-  $(".js-details-view").hide();
-  $(".js-results-view").hide();
-
   var DATA_GOV_API_KEY = "ts2aIGdCnhQMCUyO4jLJArVfM04Wwe0AehE1PsVB";
-  var SEARCH_API_URL = "https://api.nal.usda.gov/ndb/search/?format=json&sort=n&max=10&offset=0&api_key=API_KEY" + DATA_GOV_API_KEY + "&q=SEARCH_QUERY";
-  var SINGLE_FOOD_API_URL = "https://api.nal.usda.gov/ndb/search/?format=json&sort=n&max=10&offset=0&api_key=DEMO_KEY&q=cheese" + DATA_GOV_API_KEY + "&q=SEARCH_QUERY";
-//these get the nutrients we want to display //Calories, protein, fat, carb, fiber, sugars
-//produces the total amount in Calories for 1, and in grams for 4, 5, 6, 7, 8
- /* var Calories = report.food.nutrients[1].measures[0].value;
-  var protein = report.food.nutrients[3].measures[0].value;
-  var fat = report.food.nutrients[4].measures[0].value;
-  var carb = report.food.nutrients[6].measures[0].value;
-  var fiber = report.food.nutrients[7].measures[0].value;
-  var sugar = report.food.nutrients[8].measures[0].value;
-*/
-//Click on "Vitamins?" (at the bottom of nutrients grid) link to be transported to USDA page For breakdown of vitamins for that food
-// state section of the code
+  var SEARCH_API_URL = "https://api.nal.usda.gov/ndb/search/?format=json&sort=n&max=10&offset=0&api_key=" + DATA_GOV_API_KEY;
+  var SINGLE_FOOD_API_URL = "https://api.nal.usda.gov/ndb/reports/?format=json&api_key=" + DATA_GOV_API_KEY;
+
   var state = {
 
-  selectedFood: null,
-/*
-  searchResults: []
-*/
-};
-//state modification 
-  var foodData = response.report.food;
+    selectedFood: null,
 
-  var foodName = foodData.name;
+    searchResults: []
 
-  var foodNutrients = foodData.nutrients.map(function(item) {
-
-    return {
-      name: item.name,
-      value: item.measures[0].value,
   };
-});
-
-state.selectedFood = {
-
-  name: foodName,
-  nutrients: foodNutrients,
-};
-
-//state rendering functions
-function onSearch(searchQuery) {
-
-  $.getJSON(SEARCH_API_URL, function(response) {
-    state.searchResults = response.list.item;
-
-  });
-}
-//used to be...search-result as the class...does search-result need to be changed?  to "js-results-view"
 function renderSearchResult(data) {
-  return '<li class="js-results-view" data-ndbno="' + data.ndbno +
+    return '<li class="search-results-item js-search-results-item" data-ndbno="' + data.ndbno +
     '">' + data.name + '</li>';
+  }
+
+function renderNutrient(nutrientData) {
+//wrap in span to do styling for below
+    return '<li class="nutrient-item js-nutrient-item">' + nutrientData.name + " " + nutrientData.value + "</li>"
+
 }
 
-function displaySearchResults() {
-  var resultsMarkup = 
-    state.searchResults.map(renderSearchResult);
+function renderFoodDetails() {
 
-    //$(".js-hidden").hide().removeClass(hide)
-   // this isn't doing what I want it to do $(".js-hidden").hide().remove();
-   //.toggle instead?
+  var selectedFood = state.selectedFood
+  $(".js-food-name-title").html(selectedFood.name);
+  var nutrientMarkup = selectedFood.nutrients.map(renderNutrient);
+  $(".js-nutrient-details").html(nutrientMarkup.join(""));
+
 }
-//event listeners
-$('.js-results-view').on('click', function(event) {
+  function displaySearchResults() {
+    var resultsMarkup = state.searchResults.map(renderSearchResult);
+/*another way to write this: 
+    var resultsMarkupItems = state.searchResults.map(...);
+    var resultsMarkup = resultsMarkupItems.join('');*/
+    $(".js-search-results").html(resultsMarkup.join(""));
+  }
+//state rendering functions
+  function onSearch(searchQuery) {
 
+    $.getJSON(SEARCH_API_URL, { q: searchQuery }, function(response) {
+      state.searchResults = response.list.item;
+      displaySearchResults();
+    });
+  }
+  //event listeners
+  $('.js-search-form').on('submit', function(event) {
+  //this is saying when I click on GO, the follwing with happen
   event.preventDefault();
-    
-    $(".js-home-view").show();
-    $(".js-details-view").hide();
-    $(".js-results-view").show();
+
+  $(".js-search-results").removeClass("hidden");
+  $(".js-food-name-title").removeClass("hidden");
+  var $target = $(event.currentTarget);
+  var searchQuery = $target.find(".js-search-text").val();
+
+     onSearch(searchQuery);
+      //renderSearchResult(data);
+    });
+
+  $(".js-search-results").on("click", ".js-search-results-item", function(event) {
+    event.preventDefault();
     var $target = $(event.currentTarget);
     var ndbno = $target.attr('data-ndbno');
-    onSearch(searchQuery);
-    renderSearchResult(data);
+
+    $(".js-home-view").addClass("hidden");
+    $(".js-details-view").removeClass("hidden");
+    
+
+  $.getJSON(SINGLE_FOOD_API_URL, { ndbno: ndbno }, function(response) {
+     
+      var foodData = response.report.food;
+      var foodName = foodData.name;
+      var foodNutrients = foodData.nutrients.map(function(item) {
+        return {
+          name: item.name,
+          value: item.measures[0].value + " " + item.unit,
+        }
+
+      });
+
+//console.log(foodData.nutrients); //LOG THIS TO FIND THE UNIT TO ADD
+
+      state.selectedFood = {
+        name: foodName,
+        nutrients: foodNutrients,
+
+      };
+
+  renderFoodDetails();
+
     });
   });
-   
-$(".js-details-view").on("click", function(event) {
-
-    event.preventDefault();
-
-    $(".js-home-view").hide();
-    $(".js-details-view").show();
-    $(".js-results-view").hide();
-
-  $.getJSON(SINGLE_FOOD_API_URL, { ndbno: ndbno }, 
-      function(repsonse) {
-
-        var foodData = repsonse.report.food;
-        var foodName = foodData.name;
-        var foodNutrients = foodName.foodData;
-        displaySearchResults();
-
-   });
-
- /*   foodData.nutrients.map(function(item) {
-      return {
-        name: item.name,
-        value: item.measures[0].value,
-
-      }});
-
-    state.selectedFood = {
-      name: foodName,
-      nutrients: foodNutrients,
-      };
-*/
 });
-  
-   
-
-
-
-/*
-//view home has the title and the subtitle for the search, search box, submit
-//form submit handler
-//console.log
-//select form using jQuery
-//grab search bar
-  function searchUSDA () {
-// gets data from database when searched
- // }
-
- // function displayResults() {
-//Displays top 10 results
- // }
-
- // function getDetailsOfResult() {
-//gets details of food selected, when clicked
-//takes us to the 2nd view, view-details
-//has a title of the food as the main title (unhides it)
-//has 3 columns, 1st column is nutrient, 2nd is unit of measurement, 3rd is how much in the food (total, or serving i.e. 1 cup, depending on the food)
-//hides search box
-//unhides search box at top of page
-
-//  }
-
- // function loadMoreResults() {
- //gets next 10 results to the DOM that you can click on, updates the state}
-
-//APPetite Logo will remain in upper left corner always, takes u to the home screen  }
-
-}
-
-//FORM submit handler
-$(".js-search-form").on("submit", function(event) {
-  event.preventDefault();
-
-  var searchForm = $(event.currentTarget);
-
-  var searchText = searchForm.find(".js-search-text").val();
-
-  //console.log(searchText);  
-})
-
-
-
-}); */
