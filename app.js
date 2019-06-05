153bf76dd8b827f21631330248a21eb5948aa4d5
bradleymarch <bradleymarch@gmail.com>
2019-06-05 16:28:34 -0500

    another api change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(function() {
 
    var DATA_GOV_API_KEY = "ts2aIGdCnhQMCUyO4jLJArVfM04Wwe0AehE1PsVB";
-   var SEARCH_API_URL = "https://api.nal.usda.gov/ndb/search/?format=json&max=10&offset=0&api_key=" + DATA_GOV_API_KEY;
+   var SEARCH_API_URL = "https://api.nal.usda.gov/ndb/V2/search/?format=json&max=10&offset=0&api_key=" + DATA_GOV_API_KEY;
    var SINGLE_FOOD_API_URL = "https://api.nal.usda.gov/ndb/V2/reports/?format=json&api_key=" + DATA_GOV_API_KEY;
    // console.log(SINGLE_FOOD_API_URL);
    var state = {
